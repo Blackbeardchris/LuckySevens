908580ec8501e1dea53b33af56153d8ade52ec8a
Christopher Landry <christopherl10@learn.cssd.ab.ca>
2018-09-24 15:15:04 -0600

    ok tutorials done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace luckysevens
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string [] myArray = new string [] { "Learn" , "app", "development"};
+
+            int [] intArray = new int [] {232, 455, 3422, 42564};
+
+            string word = myArray[1];
+
+            Console.WriteLine(word);
+
+            int arrayLength = intArray.Length;
+
+            Console.WriteLine(arrayLength.ToString());
         }
     }
 }
